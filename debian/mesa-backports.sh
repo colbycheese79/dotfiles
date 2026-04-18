87dac96e4f503a80e3b5d02eca06beffc0e6de7f
Colby <colby.braden@gmail.com>
2026-04-18 11:26:42 -0500

    updated mesa backports for trixie

--- debian/mesa-backports.sh
+++ debian/mesa-backports.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo apt -t bookworm-backports install libegl-mesa0 libgl1-mesa-dri libglx-mesa0 mesa-libgallium mesa-va-drivers mesa-vulkan-drivers
+sudo dpkg --add-architecture i386 &&
+sudo apt update &&
+sudo apt install -t trixie-backports libegl-mesa0 libgbm1 libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers &&
+sudo apt install -t trixie-backports steam libgl1-mesa-dri:i386 mesa-vulkan-drivers:i386
