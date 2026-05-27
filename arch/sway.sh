08725a4ed4b4953e3eddf6c6be669d696c07ecd5
Colby <75183285+colbycheese79@users.noreply.github.com>
2026-05-27 00:34:09 -0500

    Update and cleaned up sway.sh

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
-sudo pacman -S --needed vim pacman-contrib sway swaylock swayidle swaybg i3status \
-    foot rofi seatd playerctl papirus-icon-theme rsync uwsm fastfetch \
-    lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db \
-    gtk-engine-murrine mpd mpv xorg-xwayland gvfs xarchiver evince simple-scan \
-    gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
-    gnome-themes-extra otf-font-awesome noto-fonts-emoji ttf-croscore ttf-dejavu ttf-droid \
-    thunar thunar-archive-plugin thunar-volman tumbler fuse geany galculator \
-    gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family \
-    nwg-look autotiling materia-gtk-theme nwg-look noto-fonts-emoji fwupd \
-    xdg-desktop-portal-gtk xdg-desktop-portal-wlr lxsession &&
-# Cups
-sudo systemctl enable cups &&
-sudo systemctl start cups
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Updating system and installing Sway environment..."
+
+# Organize packages into readable categories
+PACKAGES=(
+    # --- Core Wayland & Sway ---
+    sway swaylock swayidle swaybg i3status autotiling uwsm seatd wl-clipboard 
+    xorg-xwayland xdg-desktop-portal-gtk xdg-desktop-portal-wlr
+
+    # --- System Utilities & Daemons ---
+    pacman-contrib rsync fastfetch polkit-gnome fwupd man-db cups ipp-usb ghostscript
+
+    # --- Audio & Media ---
+    pipewire pipewire-pulse wireplumber playerctl mpd mpv
+
+    # --- Terminal & Shell Tools ---
+    foot vim
+
+    # --- Application Launcher & Notifications ---
+    fuzzel dunst
+
+    # --- File Manager & Core Desktop Apps ---
+    thunar thunar-archive-plugin thunar-volman tumbler gvfs xarchiver 
+    papers simple-scan flatpak galculator geany imv
+
+    # --- Look & Feel (GTK, Icons, Themes) ---
+    lxappearance nwg-look materia-gtk-theme gnome-themes-extra 
+    gtk-engine-murrine papirus-icon-theme libadwaita
+
+    # --- Fonts ---
+    otf-font-awesome noto-fonts-emoji ttf-croscore ttf-dejavu ttf-droid 
+    gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family
+)
+
+# Install packages
+sudo pacman -S --needed --noconfirm "${PACKAGES[@]}"
+
+# --- Post-Installation Configuration ---
+
+echo "Enabling and starting system services..."
+
+# Enable and start CUPS (Printing) in a single command
+sudo systemctl enable --now cups.service
+
+echo "Installation complete! You can now launch Sway via UWSM using: uwsm start sway-session"
